24ec3ee78da58aa1a3c321c3bacd813b9b23a815
SahilPanhotra <sahilpanhotra@gmail.com>
2022-07-12 19:09:04 +0530

    basic Smart Contract

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,2 +1,15 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+
+//Console functions to help debug the smart contract just like in Javascript
+import "hardhat/console.sol";
+//OpenZeppelin's NFT Standard Contracts. We will extend functions from this in our implementation
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        owner = payable(msg.sender);
+    }
+}
